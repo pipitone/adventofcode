7403a1e46979cba3a1d9402f975b77c91f332a8f
Jon Pipitone <jon@pipitone.ca>
2019-12-01 17:17:06 -0500

    Tidy

--- 2019/day1-1.py
+++ 2019/day1-1.py
@@ -1,10 +1,8 @@
-import math
-
 def get_fuel(mass): 
-    fuel = math.floor(mass/3.0) - 2
+    fuel = int(mass)//3.0 - 2
     return fuel
 
 print reduce(
         lambda fuel, mass: get_fuel(mass) + fuel, 
-        map(int, open('day1-input')), 
+        open('day1-input'), 
         0)
--- 2019/day1-2.py
+++ 2019/day1-2.py
@@ -1,10 +1,8 @@
-import math
-
 def get_fuel(mass): 
-    fuel = math.floor(mass/3.0) - 2
+    fuel = int(mass)//3 - 2
     return 0 if (fuel < 0) else (fuel + get_fuel(fuel))
 
 print reduce(
         lambda fuel, mass: get_fuel(mass) + fuel, 
-        map(int, open('day1-input')), 
+        open('day1-input'), 
         0)
